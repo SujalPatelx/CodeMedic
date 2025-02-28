c93b391093c934029cc2edcfa3852df03863b9d4
Manish kumar <134037752+Manish-kumar-9601@users.noreply.github.com>
2025-02-28 14:29:35 +0530

    add CodeEditor component and integrate Tailwind CSS; refactor App component
    Signed-off-by: Manish kumar <134037752+Manish-kumar-9601@users.noreply.github.com>

---
+++ client/src/components/Editor.jsx
@@ -0,0 +1,13 @@
+﻿import Editor, { DiffEditor, useMonaco, loader } from '@monaco-editor/react';
+import React from 'react'
+
+export const CodeEditor = () =>
+{
+    return (
+
+        <>
+            <Editor height="90vh" defaultLanguage="javascript" defaultValue="// some comment" />
+        </>
+    )
+}
+ 
